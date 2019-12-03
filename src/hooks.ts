8116ee248128a8c828f2e7ca34caba8ef7722d9b
Richard Hidalgo <hidalgo931@gmail.com>
2019-12-03 16:59:14 -0300

    feat: export hooks useFormatNumber, useFormatDate and useFormatTime (#3)

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,5 +1,12 @@
 import { useContext } from 'react';
-import { FormattedMessage, MessageValue } from 'react-intl';
+import {
+  DateSource,
+  FormattedDate,
+  FormattedMessage,
+  FormattedNumber,
+  FormattedTime,
+  MessageValue
+} from 'react-intl';
 import { IntlContext } from './IntlContext';
 
 export type FormatMessage = (
@@ -14,4 +21,46 @@ export const useFormatMessage = () => {
     formatMessage({ id }, values);
 
   return formatMessageFn;
+};
+
+export type FormatNumber = (
+  value: number,
+  options?: FormattedNumber.PropsBase
+) => string;
+
+export const useFormatNumber = () => {
+  const { formatNumber } = useContext(IntlContext);
+
+  const formatNumberFn: FormatNumber = (value, options) =>
+    formatNumber(value, options);
+
+  return formatNumberFn;
+};
+
+export type FormatDate = (
+  value: DateSource,
+  options?: FormattedDate.PropsBase
+) => string;
+
+export const useFormatDate = () => {
+  const { formatDate } = useContext(IntlContext);
+
+  const formatDateFn: FormatDate = (value, options) =>
+    formatDate(value, options);
+
+  return formatDateFn;
+};
+
+export type FormatTime = (
+  value: DateSource,
+  options?: FormattedTime.PropsBase
+) => string;
+
+export const useFormatTime = () => {
+  const { formatTime } = useContext(IntlContext);
+
+  const formatTimeFn: FormatTime = (value, options) =>
+    formatTime(value, options);
+
+  return formatTimeFn;
 };
